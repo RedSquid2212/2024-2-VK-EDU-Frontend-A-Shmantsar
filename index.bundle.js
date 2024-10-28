e0a35b09c5f691c7238a8cdf23758206ffead1eb
RedSquid2212 <RedSquid2212@users.noreply.github.com>
2024-10-28 15:44:11 +0000

    deploy: eb1bd4bd2d9e9ecbd649714e4d372f75c160ad18

--- index.bundle.js
+++ index.bundle.js
@@ -81,31 +81,42 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./scripts/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./index.css":
-/*!*******************!*\
-  !*** ./index.css ***!
-  \*******************/
+/***/ "./scripts/chat-list.js":
+/*!******************************!*\
+  !*** ./scripts/chat-list.js ***!
+  \******************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./index.css?");
+eval("var chatButton = document.querySelector('.main-page__chat');\nchatButton.addEventListener('click', function () {\n  window.location.href = 'http://localhost:8080/chat.html';\n});\n\n//# sourceURL=webpack:///./scripts/chat-list.js?");
 
 /***/ }),
 
-/***/ "./index.js":
-/*!******************!*\
-  !*** ./index.js ***!
-  \******************/
+/***/ "./scripts/index.js":
+/*!**************************!*\
+  !*** ./scripts/index.js ***!
+  \**************************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ \"./index.css\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_0__);\nvar _JSON$parse;\n\nvar form = document.querySelector('form');\nvar input = document.querySelector('.message-input');\nvar messageList = document.querySelector('.message-list');\nvar oldMessages = (_JSON$parse = JSON.parse(localStorage.getItem('messages'))) !== null && _JSON$parse !== void 0 ? _JSON$parse : [];\nwindow.addEventListener('load', function () {\n  oldMessages.forEach(displayMessage);\n});\nwindow.addEventListener('unload', function () {\n  return saveInLocalStorage();\n});\nform.addEventListener('submit', handleSubmit);\nform.addEventListener('keypress', handleKeyPress);\nfunction handleSubmit(event) {\n  event.preventDefault();\n  var messageText = input.value.trim();\n  if (messageText && messageText.length > 0) {\n    var message = {\n      text: messageText,\n      from: 'Jennifer',\n      time: new Date().toLocaleTimeString('RU', {\n        hour: '2-digit',\n        minute: '2-digit'\n      })\n    };\n    oldMessages.push(message);\n    displayMessage(message);\n    input.value = '';\n  }\n}\nfunction handleKeyPress(event) {\n  if (event.keyCode === 13 && !event.shiftKey) {\n    form.dispatchEvent(new Event('submit'));\n  }\n}\nfunction displayMessage(message) {\n  var messageElement = document.createElement('div');\n  messageElement.classList.add('message');\n  messageElement.innerText = \"\".concat(message.text);\n  messageElement.insertAdjacentHTML('beforeend', \"<span class=\\\"message-time\\\">\".concat(message.from, \" at \").concat(message.time, \"</span>\"));\n  messageList.appendChild(messageElement);\n}\nfunction saveInLocalStorage(message) {\n  localStorage.setItem('messages', JSON.stringify(oldMessages));\n}\n\n//# sourceURL=webpack:///./index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../styles/index.css */ \"./styles/index.css\");\n/* harmony import */ var _styles_index_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_styles_index_css__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _chat_list__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./chat-list */ \"./scripts/chat-list.js\");\n/* harmony import */ var _chat_list__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_chat_list__WEBPACK_IMPORTED_MODULE_1__);\n\n\n\n//# sourceURL=webpack:///./scripts/index.js?");
+
+/***/ }),
+
+/***/ "./styles/index.css":
+/*!**************************!*\
+  !*** ./styles/index.css ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./styles/index.css?");
 
 /***/ })
 
